3cb4b3d207bf6119e658cef44ce04761642c0c86
Bruna Radeke <brunaradeke@hotmail.com>
2020-06-03 20:37:23 +0100

    Animations - Wrapping the Transition Component (24-402)
    Wrapping the Transition inside the Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition'; //can be any name for the component
 
 import './Modal.css';
 
 const modal = (props) => {
-    const cssClasses = [
-        "Modal",
-        props.show === "entering" //From state transition
-            ? "ModalOpen"
-            : props.show === "exiting" ? "ModalClosed" : null
-    ];
-
     return (
-        <div className={cssClasses.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
+        <Transition
+            mountOnEnter
+            unmountOnExit
+            in={props.show}
+            timeout={300}>
+            {state => {
+                const cssClasses = [
+                    "Modal",
+                    state === "entering" //From state transition
+                        ? "ModalOpen"
+                        : state === "exiting" ? "ModalClosed" : null
+                ];
+                return (
+                    <div className={cssClasses.join(' ')}>
+                        <h1>A Modal</h1>
+                        <button className="Button" onClick={props.closed}>Dismiss</button>
+                    </div>
+                )
+            }}
+        </Transition>
+
     );
 };
 
