ba7e0cf09f44844fc443dfc8634991c80d897be4
Bruna Radeke <brunaradeke@hotmail.com>
2020-06-03 20:18:15 +0100

    Animations - Using ReactTransitionGroup (24-400)

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Animations in React Apps
 
+## React Transition Group Package
+It is a package created by the vibrant react community and this is a package which allows you to smoothly animate elements when they are added and removed to and from the dom.
+
+[https://github.com/reactjs/react-transition-group](https://github.com/reactjs/react-transition-group)
+
+
+```sh
+yarn add react-transition-group
+```
+#### Transition Component Example
+```jsx
+import Transition from 'react-transition-group/Transition'; //can be any name for the component
+
+<Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+          {/* {state => <p>{state}</p>} 
+              To see the state entering/entered VS exiting/exited */}
+          {state => (
+            <div style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "10px auto",
+              transition: "opacity 1s ease-out",
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+          )}
+        </Transition>
+```
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition'; //can be any name for the component
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -8,7 +9,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -23,6 +25,30 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() =>
+          this.setState(prevState => ({ showBlock: !prevState.showBlock }))}
+        >Toggle</button>
+        <br />
+        {/* to show and remove element from the dom 
+            mountOnEnter and unmountOnExit */}
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+          {/* {state => <p>{state}</p>} 
+              To see the state entering/entered VS exiting/exited */}
+          {state => (
+            <div style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "10px auto",
+              transition: "opacity 1s ease-out",
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+          )}
+        </Transition>
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
